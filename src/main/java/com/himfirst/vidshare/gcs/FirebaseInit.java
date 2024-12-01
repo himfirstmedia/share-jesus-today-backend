751f536911561be76d20ca4c8f8fd0d451e6945b
lodkazibwe <lodkazibwe@gmail.com>
2024-12-01 18:33:10 +0300

    gcs config

---
+++ src/main/java/com/himfirst/vidshare/gcs/FirebaseInit.java
@@ -0,0 +1,30 @@
+package com.himfirst.vidshare.gcs;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Service
+
+public class FirebaseInit {
+    @PostConstruct
+    public void initialize() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("/home/qci_user/apps/v2/QCiStaffSacco/serviceAcount.json");// /home/qci_user/apps/v2/QCiStaffSacco
+        //  ./serviceAcount.json
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://qc-sacco-default-rtdb.firebaseio.com")
+                .setStorageBucket("qc-sacco.appspot.com")
+                .build();
+
+            FirebaseApp.initializeApp(options);
+
+    }
+}
---
+++ src/main/java/com/himfirst/vidshare/gcs/file/FileService.java
@@ -0,0 +1,76 @@
+package com.himfirst.vidshare.gcs.file;
+
+import com.google.firebase.cloud.StorageClient;
+import com.himfirst.vidshare.security.MyUserDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Date;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class FileService {
+    @Autowired
+    MyUserDetailsService myUserDetailsService;
+
+    private final Logger logger = LoggerFactory.getLogger(FileService.class);
+
+    public String uploadImage(MultipartFile myFile, String userName) throws IOException {
+
+        logger.info("converting image....");
+        InputStream file =  new BufferedInputStream(myFile.getInputStream());
+        logger.info("getting image name....");
+        String fileName=generateFileName(myFile);
+        String name="qcProfile/"+userName+"/"+ fileName;
+        logger.info("uploading image....");
+        StorageClient.getInstance().bucket()
+                .create(name, file);
+        logger.info("updating profile....");
+        //return bucket(name);
+        return name;
+
+   }
+
+    public String uploadFile(MultipartFile myFile, String userName) throws IOException {
+
+        logger.info("converting image....");
+        InputStream file =  new BufferedInputStream(myFile.getInputStream());
+        logger.info("getting image name....");
+        String fileName=generateFileName(myFile);
+        String name="qcFile/"+userName+"/"+ fileName;
+        logger.info("uploading image....");
+        StorageClient.getInstance().bucket()
+                .create(name, file);
+        logger.info("updating profile....");
+        return name;
+
+    }
+
+    private String generateFileName(MultipartFile multiPart) {
+
+        return new Date().getTime() + "-" + Objects.requireNonNull(multiPart.getOriginalFilename())
+                .replace(" ", "_");
+    }
+
+    public URL signedUrl(String imageName) {
+       logger.info("getting image....");
+        StorageClient.getInstance().bucket().get(imageName).getBucket();
+        return StorageClient.getInstance().bucket().get(imageName).signUrl(14, TimeUnit.DAYS);
+
+    }
+
+    public String bucket(String imageName) {
+        logger.info("getting image....");
+        return StorageClient.getInstance().bucket().get(imageName).getMediaLink();
+
+    }
+
+}
